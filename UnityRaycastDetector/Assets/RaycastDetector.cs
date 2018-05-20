4533384a5e6afb20f979726fe95b9ef29ea1add3
Goropocha <>
2018-05-20 20:33:19 +0900

    Add Sample project

---
+++ UnityRaycastDetector/Assets/RaycastDetector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// Simple raycast detector
+/// </summary>
+public class RaycastDetector : Graphic {
+
+  /// <summary>
+  /// A utility class that can aid in the generation of meshes for the UI.
+  /// </summary>
+  /// <param name="vh">VertexHelper</param>
+  protected override void OnPopulateMesh(VertexHelper vh) {
+    // Clear all vertices from the stream.
+    vh.Clear();
+  }
+
+#if UNITY_EDITOR
+  [CustomEditor(typeof(RaycastDetector))]
+  public class GraphicCastEditor : Editor {
+    // Nothing show to Inspector
+    public override void OnInspectorGUI() {}
+  }
+#endif
+}
---
+++ UnityRaycastDetector/Assets/SampleRaycastDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SampleRaycastDetector : MonoBehaviour {
+
+  [SerializeField] private Button buttonWithImage;
+  [SerializeField] private Button buttonNoImage;
+
+  [SerializeField, Header("Output Panel")] private Text infoPanelText;
+
+  // Use this for initialization
+  void Start() {
+    buttonWithImage.onClick.AddListener(() => {
+      infoPanelText.text = "Button With Image!\n";
+    });
+    buttonNoImage.onClick.AddListener(() => {
+      infoPanelText.text = "No Image Button!\n";
+    });
+  }
+}
